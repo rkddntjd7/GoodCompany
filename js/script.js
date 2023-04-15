a1caf3539553dccb74f8873fb7db568b9b0d44e3
rkddntjd7 <rkddntjd7@naver.com>
2023-04-15 21:39:15 +0900

    0415 21:39

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(function(){
         $(this).find('.lnb').stop().slideToggle();
         $('.background').stop().slideToggle();
     });
+
+    $('.slidein').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
 }); // jquery
